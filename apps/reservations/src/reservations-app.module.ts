04c544557894b2f7816e9e6f1c15a64f39df2707
YhomiAce <kareemyomi91@gmail.com>
2024-09-24 22:30:01 +0100

    authentication with passport local

--- apps/reservations/src/reservations-app.module.ts
+++ apps/reservations/src/reservations-app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ReservationsModule } from './reservations/reservations.module';
 import { DatabaseModule, LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from "joi";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: "../env",
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      })
+    }),
     DatabaseModule,
     ReservationsModule,
     LoggerModule
